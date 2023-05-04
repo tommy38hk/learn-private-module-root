5da5d66a4157c0138cfc6b996c37205fc33d47f5
Tommy Wu <twu@solifi.com>
2023-05-04 08:31:46 -0400

    Removed duplicated provider

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.21.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "4.21.0"
+#    }
+#  }
+#}
