77694102d7b34be490d6477f5a1d6f8d456a94fb
Tommy Wu <twu@solifi.com>
2023-05-04 08:25:49 -0400

    Set private module

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
-
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/example-org-8bf983/s3-webapp/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
